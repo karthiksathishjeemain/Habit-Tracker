852a26b368c6876e02a47977146b1ee3fdc3b1ab
KARTHIK SATHISH <karthiksathishjeemain@gmail.com>
2024-12-26 12:44:53 +0530

    Resolved the import of UserProfile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,16 @@ import  { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate, useNavigate } from 'react-router-dom';
 import Login from "./components/auth/Login"
 import Register  from './components/auth/Register';
-import { UserProfile } from './components/auth/userprofile';
+import { UserProfile } from './components/auth/UserProfile';
 import Dashboard from './components/Dashboard';
 
 function App() {
   // const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
-  const NavigateToLogin =()=> {
-    const navigate = useNavigate();
-    navigate("/login");
-  }
+  // const NavigateToLogin =()=> {
+  //   const navigate = useNavigate();
+  //   navigate("/login");
+  // }
   const handleLoginSuccess = () => {
     setIsAuthenticated(true);
   };
