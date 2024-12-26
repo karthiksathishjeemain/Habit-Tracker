eac688d5032f8b12a69088acc7e1636ce01f8f4b
KARTHIK SATHISH <karthiksathishjeemain@gmail.com>
2024-12-26 21:41:51 +0530

    cloud sql connection is set

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Karthik@1234',
-    database: 'habit_tracker',
-    
+   host: process.env.DB_HOST, // Public IP of your Cloud SQL instance
+    user: process.env.DB_USER, // Usually "root"
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 const promisePool = pool.promise();
